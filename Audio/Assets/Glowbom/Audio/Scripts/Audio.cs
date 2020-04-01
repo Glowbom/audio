3652aec9bd7fa16f1fef3ed064a87745926f2ce6
Jacob Ilin <me@jacobilin.com>
2020-04-01 16:58:05 -0600

    Audio script added

---
+++ Audio/Assets/Glowbom/Audio/Scripts/Audio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Created on Wed Apr 1 2020
+ *
+ * Copyright (c) 2020 Glowbom, Inc.
+ */
+public class Audio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
