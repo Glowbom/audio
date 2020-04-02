d2f9a659f6b88de54ae6bee0edd67890d75d4391
Jacob Ilin <me@jacobilin.com>
2020-04-01 18:46:57 -0600

    Getting all resources

--- Audio/Assets/Glowbom/Audio/Scripts/Audio.cs
+++ Audio/Assets/Glowbom/Audio/Scripts/Audio.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 /*
@@ -12,7 +13,14 @@ public class Audio : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Find all Texture2Ds that have 'co' in their filename, that are labelled with 'architecture' and are placed in 'MyAwesomeProps' folder
+        // to get all textures user t:texture2D
+        string[] files = AssetDatabase.FindAssets("", new[] { "Assets/Glowbom/Audio/Resources" });
+
+        foreach (string guid in files)
+        {
+            Debug.Log(AssetDatabase.GUIDToAssetPath(guid));
+        }
     }
 
     // Update is called once per frame
