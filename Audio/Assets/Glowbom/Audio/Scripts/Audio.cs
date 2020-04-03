6dbb04f24329702a3a72cc1e8a2d00beed3812df
Jacob Ilin <me@jacobilin.com>
2020-04-02 18:47:17 -0600

    Playing logic

--- Audio/Assets/Glowbom/Audio/Scripts/Audio.cs
+++ Audio/Assets/Glowbom/Audio/Scripts/Audio.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
 using UnityEngine;
+using UnityEngine.UI;
 
 /*
  * Created on Wed Apr 1 2020
@@ -16,11 +18,15 @@ using UnityEngine;
 public class Audio : MonoBehaviour
 {
     public AudioSource audioSource;
+    public AudioDataLoader audioDataLoader;
+    public Dictionary<string, Sprite> sprites;
+    public Dictionary<string, List<string>> imageKeys;
+    public Image image;
 
     // Start is called before the first frame update
     void Start()
     {
-        AudioDataLoader audioDataLoader = new AudioDataLoader();
+        audioDataLoader = new AudioDataLoader();
         audioDataLoader.audioData = new AudioData();
         audioDataLoader.audioData.sounds = new List<string>();
         audioDataLoader.audioData.keys = new List<string>();
@@ -63,8 +69,100 @@ public class Audio : MonoBehaviour
 
         audioDataLoader.save();
 #endif
-
         audioDataLoader.load();
+
+        loadResources();
+
+        play("cat");
+
+    }
+
+    string currentKey;
+    List<string> currentImages;
+    int currentMin = 0;
+    int currentSec = 0;
+    int currentImageIndex = 0;
+
+    private async void next()
+    {
+        Debug.Log(currentMin + ":" + currentSec);
+        if (currentImageIndex < currentImages.Count)
+        {
+            if (currentImageIndex < currentImages.Count - 1)
+            {
+                string timeKey = currentMin.ToString("00") + "_" + currentSec.ToString("00");
+                Debug.Log("timeKey: " + timeKey);
+                int timeIndex = audioDataLoader.audioData.pictures.IndexOf(currentImages[currentImageIndex + 1]);
+
+                if (timeKey.Equals(audioDataLoader.audioData.times[timeIndex]))
+                {
+                    ++currentImageIndex;
+                }
+            }
+
+            image.sprite = sprites[currentImages[currentImageIndex]];
+        }
+        
+        if (audioSource.isPlaying)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            ++currentSec;
+            if (currentSec >= 60)
+            {
+                currentSec = 0;
+                ++currentMin;
+            }
+            StartCoroutine("next");
+        }
+    }
+
+    public void play(string key)
+    {
+        currentKey = key;
+        currentImages = imageKeys[key];
+        currentMin = 0;
+        currentSec = 0;
+        currentImageIndex = 0;
+
+        StartCoroutine("next");
+    }
+
+    private void loadResources()
+    {
+        sprites = new Dictionary<string, Sprite>();
+
+        foreach (string path in audioDataLoader.audioData.pictures)
+        {
+            if (!sprites.ContainsKey(path))
+            {
+                string[] stringSeparators = { "Resources/" };
+                string[] parts = path.Split(stringSeparators, StringSplitOptions.None);
+                sprites.Add(path, Resources.Load<Sprite>(parts[parts.Length - 1].Replace(".png", "").Replace(".jpg", "")));
+
+            }
+        }
+
+        imageKeys = new Dictionary<string, List<string>>();
+
+
+        foreach (string key in audioDataLoader.audioData.keys)
+        {
+            if (!imageKeys.ContainsKey(key))
+            {
+                imageKeys.Add(key, new List<string>());
+            }
+
+            foreach (string path in audioDataLoader.audioData.pictures)
+            {
+                if (path.Contains(key))
+                {
+                    if (!imageKeys[key].Contains(path))
+                    {
+                        imageKeys[key].Add(path);
+                    }
+                }
+            }
+        }
     }
 
     // Update is called once per frame
